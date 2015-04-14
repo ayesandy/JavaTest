61495f30559b2fd654bda3a506fcd32b04aa6b7a
azwin11 <aungzaanwinn@gmail.com>
2015-04-13 20:45:51 -0700

    Added the folder Hello

---
+++ Hello/src/Hello.java
@@ -0,0 +1,32 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		
+		//int x = 50;
+		String name = "Fido";
+		
+	   // class Dog {
+		//Dog myDog = new Dog (name, size);
+	   // }
+		//int x = size - 5;
+		
+		for ( int x=50; x > 5; x-=10){
+		if (x > 15) 
+			System.out.println("My Dog Information = " + name + x);
+		
+		
+		//while (x< 3) {
+		else 
+			System.out.println("sorry Ma Sandy ... ");
+			
+		//for (int x, x>25, x++){
+			//System.out.println("My Dog Information=" + name + size);
+		//	System.out.println("sorry Ma Sandy...");
+			
+		}
+		
+	}
+}
+
+
